70fa084518e12ed059b34ac88ebae19987f0de51
manish0521 <manish.singh@codeimmersives.com>
2019-11-25 13:03:00 -0500

    edits with comment

--- myProject-Frontend/src/Factory/MessageBar/MessageBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MessageBar = (props) => {
-  return (
-    <h3 className={props.dynamicClassName} style={{color: props.fontColorStyle}}>{props.children}</h3>
-  )
-}
-
-export default MessageBar;
